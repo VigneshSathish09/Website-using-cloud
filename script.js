16fa0af244bf81216e9212753bf028d73b21c539
VigneshSathish09 <vigneshsathish.09@gmail.com>
2026-07-16 21:25:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function hello(){
+
+    alert(" Heya! Welcome to my website. 😁");
+}
+
+let dark = false;
+
+function changeTheme(){
+
+    if(!dark){
+
+        document.body.style.background =
+        "linear-gradient(135deg,#141E30,#243B55)";
+
+    }else{
+
+        document.body.style.background =
+        "linear-gradient(135deg,#4facfe,#00f2fe)";
+    }
+
+    dark = !dark;
+}
